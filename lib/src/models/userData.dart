83236bcdb33f18857c995402c772c3820db9547f
devmoses <moses.thipparthi@yahoo.com>
2019-12-18 22:30:26 +0530

    Add model for user(Job Seeker) data

--- lib/src/models/userData.dart
+++ lib/src/models/userData.dart
@@ -11,55 +11,50 @@ class UserData {
   String _username = "";
   String _password = "";
 
-  String get username => _username;
-
-  void setUsername(String value) {
-    _username = value;
-  }
-
   @override
   String toString() {
-    return 'userData{_firstName: $_firstName, _lastName: $_lastName, _storeName: $_storeName, _addressLine: $_addressLine, _city: $_city, _state: $_state, _pinCode: $_pinCode, _contact: $_contact, _email: $_email}';
+    return 'userData {'
+        '_firstName: $_firstName, '
+        '_lastName: $_lastName, '
+        '_storeName: $_storeName, '
+        '_addressLine: $_addressLine, '
+        '_city: $_city, '
+        '_state: $_state, '
+        '_pinCode: $_pinCode,'
+        ' _contact: $_contact, '
+        '_email: $_email }';
   }
+
+  // All setters must be declared here
 
   void setFullName(String fn, String ln) {
     _firstName = fn;
     _lastName = ln;
   }
 
-  String get firstName => _firstName;
+  void setUsername(String value) {
+    _username = value;
+  }
 
   void setFirstName(String value) {
     _firstName = value;
   }
 
-  String get lastName => _lastName;
-
   void setLastName(String value) {
     _lastName = value;
   }
-
-  String get addressLine => _addressLine;
 
   void setAddressLine(String value) {
     _addressLine = value;
   }
 
-  String get city => _city;
-
   void setCity(String value) {
     _city = value;
   }
 
-  String get state => _state;
-
   void setState(String value) {
     _state = value;
   }
-
-  String get pinCode => _pinCode;
-
-  String get storeName => _storeName;
 
   void setStoreName(String value) {
     _storeName = value;
@@ -69,21 +64,39 @@ class UserData {
     _pinCode = value;
   }
 
-  String get contact => _contact;
-
   void setContact(String value) {
     _contact = value;
   }
-
-  String get email => _email;
 
   void setEmail(String value) {
     _email = value;
   }
 
-  String get password => _password;
-
   void setPassword(String value) {
     _password = value;
   }
+
+  // All getters must be declared here
+
+  String get username => _username;
+
+  String get firstName => _firstName;
+
+  String get lastName => _lastName;
+
+  String get addressLine => _addressLine;
+
+  String get city => _city;
+
+  String get state => _state;
+
+  String get pinCode => _pinCode;
+
+  String get storeName => _storeName;
+
+  String get contact => _contact;
+
+  String get email => _email;
+
+  String get password => _password;
 }
