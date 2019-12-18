d9323d44d347cdb54fee3cb4a6e3a1b850302726
devmoses <moses.thipparthi@yahoo.com>
2019-12-18 18:34:26 +0530

    Refactor main.dart to KISS principle

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:proseekr/FileUploadTest.dart';
-//import 'signin_up.dart';
-enum SelectedActor { JobProvider, JobSeeker }
-void main() => runApp(FileUploadTest());
-//
-//class ProSeekerApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'ProSeekr',
-//      theme:ThemeData(
-//        primaryColor: Colors.grey[850],
-//        accentColor: Colors.amber,
-//      ),
-//      debugShowCheckedModeBanner: false,
-//      home: FileTestUpload(title: 'Home Page'),
-//    );
-//  }
-//}
-//
-//class psHomePage extends StatefulWidget {
-//  psHomePage({Key key, this.title}) : super(key: key);
-//
-//  final String title;
-//
-//  @override
-//  _psHomePageState createState() => _psHomePageState();
-//}
-//
-//class _psHomePageState extends State<psHomePage> {
-//
-//  final List<String> _availableLanguages = ['English', 'हिन्दी', 'తెలుగు'];
-//
-//  var preferredLang = 'English';
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      backgroundColor: Colors.amber[200],
-//      body: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.start,
-//          children: <Widget>[
-//            Image.asset(
-//              'assets/images/children_with_bananas_lq.jpg',
-//            ),
-//            Padding(padding: EdgeInsets.only(bottom: 24.0)),
-//            Text(
-//              'ProSeekr',
-//              style: TextStyle(fontSize: 48),
-//            ),
-//            Text(
-//              'A job portal for blue-collars',
-//              style: TextStyle(fontSize: 18),
-//            ),
-//            Padding(padding: EdgeInsets.only(bottom: 24.0)),
-//            Text(
-//              'Select your language',
-//              style: TextStyle(fontSize: 24),
-//            ),
-//            Expanded(
-//              child: ListView.builder(
-//                  itemCount: _availableLanguages.length,
-//                  itemBuilder: (context, index) {
-//                    return Card(
-//                        elevation: 0,
-//                        shape: ContinuousRectangleBorder(),
-//                        margin: EdgeInsets.all(0),
-//                        color: index % 2 == 0
-//                            ? Colors.amber[500]
-//                            : Colors.amber[400],
-//                        child: ListTile(
-//                          contentPadding: EdgeInsets.only(left: 24),
-//                          title: Text(
-//                            _availableLanguages[index],
-//                            style: TextStyle(fontSize: 20),
-//                          ),
-//                          onTap: () {
-//                            print(preferredLang);
-//                            preferredLang = _availableLanguages[index];
-//                            Navigator.push(
-//                              context,
-//                              MaterialPageRoute(builder: (context) => psSignupin()),
-//                            );
-//
-//                          },
-//                        ));
-//                  }),
-//            ),
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
-//
+import 'package:proseekr/src/ui/PsLanguagePreferences.dart';
+
+void main() => runApp(ProSeekerApp());
+
+class ProSeekerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ProSeekr',
+      debugShowCheckedModeBanner: false,
+      home: PsLanguagePreferences(title: 'Home Page'),
+    );
+  }
+}
