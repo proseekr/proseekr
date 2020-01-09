6516a3a8134f946fb7e1302c0ac2d7047743a0f1
devmoses <moses.thipparthi@yahoo.com>
2020-01-09 18:00:02 +0530

    Update globals.dart

--- lib/src/models/globals.dart
+++ lib/src/models/globals.dart
@@ -3,17 +3,15 @@ library proseekr.globals;
 import 'package:proseekr/src/models/userData.dart';
 
 //application-wide data models
-ProviderData obj = ProviderData();
+ProviderData provider = ProviderData();
 SeekerData seeker = SeekerData();
 
 // application-wide constants
 enum SelectedActor { JobProvider, JobSeeker, None }
-//final FirebaseUser n = user;
-const String STORAGE_BUCKET_URL = 'gs://b4di-b4d4c.appspot.com';
 
 // application-wide styles
 const double PADDING = 8.0;
 
 String loggedInActor = "";
 String userId = "";
-bool newMessage = false;
+bool hasNewMessage = false;
